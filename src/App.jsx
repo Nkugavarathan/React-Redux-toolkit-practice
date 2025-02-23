f7642329c35644bcdb3d9f6a0bf8ee717a376795
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-23 14:01:13 +0530

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
-import Products from "./components/Products"
+
+import {
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+} from "react-router-dom"
+
+import Cart from "./components/Cart"
+import Dashboard from "./components/Dashboard"
+import RootLayout from "./components/RootLayout"
 
 export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
+    )
+  )
   return (
     <div>
-      <Products />
+      <RouterProvider router={router} />
     </div>
   )
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Products from "./Products"
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Products />
+    </div>
+  )
+}
