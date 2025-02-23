c20d743ce5ede9381dc83c90b27b48005fa10c74
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-23 11:19:28 +0530

    install react bootstrap package

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,14 +3,15 @@ import React, { useState, useEffect } from "react"
 export default function Products() {
   const [products, getProducts] = useState([])
   useEffect(() => {
+    //api
     fetch("https://fakestoreapi.com/products")
       .then((data) => data.json())
       .then((result) => getProducts(result))
   }, [])
   return (
     <div>
-      Products hi
-      {/* {JSON.stringify(products)} */}
+      Products
+      {/* check fetch or not  console inside useEffect or this method  {JSON.stringify(products)} */}
     </div>
   )
 }
