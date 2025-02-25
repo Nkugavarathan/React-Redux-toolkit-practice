747ba38acd5ff95f5f655008b0a38e17a40426e4
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-25 09:43:19 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,14 @@ import {
 import Cart from "./components/Cart"
 import Dashboard from "./components/Dashboard"
 import RootLayout from "./components/RootLayout"
-
+import Products from "./components/Products"
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Dashboard />} />
         <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
       </Route>
     )
   )
--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
-
+import Navbarr from "./Navbarr"
 export default function RootLayout() {
   return (
     <div>
-      RootLayout
-      <Outlet />
+      <Navbarr />
+      <div>
+        {" "}
+        <Outlet />
+      </div>
     </div>
   )
 }
