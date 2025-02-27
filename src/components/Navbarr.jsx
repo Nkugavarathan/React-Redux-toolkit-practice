0cc71ff2ada7feca3bb43790fe4b2efe8533ae23
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-27 09:32:32 +0530

    scroll bar effect removed

--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -1,53 +1,29 @@
 import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
-import { Link } from "react-router-dom"
-import { useSelector } from "react-redux"
 
 function Navbarr() {
   const countProduct = useSelector((state) => state.cart)
-  const [scrolling, setScrolling] = useState(false)
-  const [hidden, setHidden] = useState(false)
-  let lastScrollTop = 0
-
-  useEffect(() => {
-    const handleScroll = () => {
-      let scrollTop = window.scrollY
-
-      if (scrollTop > lastScrollTop && scrollTop > 50) {
-        setHidden(true) // Hide navbar when scrolling down
-      } else {
-        setHidden(false) // Show navbar when scrolling up
-      }
-
-      setScrolling(scrollTop > 50) // Change background after scrolling 50px
-      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
-    }
-
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [])
 
   return (
-    <Navbar
-      expand="lg"
-      className={`navbar-custom ${scrolling ? "scrolled" : ""} ${
-        hidden ? "hidden" : ""
-      }`}
-    >
+    <Navbar>
       <Container fluid>
         <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse>
-          <Nav.Link to="/" as={Link}>
-            Products
-          </Nav.Link>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0">
+            <Nav.Link as={Link} to="/">
+              Products
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
 
         <Navbar.Collapse className="justify-content-end me-2">
           <Navbar.Text>
-            <Nav.Link to="cart" as={Link}>
+            <Nav.Link as={Link} to="cart">
               {/* // The "as" prop makes it render as a React Router Link component.
               // This enables client-side navigation without a full page reload. */}
               My Bag {countProduct.length}
