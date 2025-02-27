a0169d8836975fcbcf2fb8c0d8c6daf95f25324f
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-27 09:25:01 +0530

    useSelector hook use

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { useSelector } from "react-redux"
 export default function Cart() {
+  const productCart = useSelector((state) => state.cart)
   return <div>Cart</div>
 }
--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -1,12 +1,41 @@
+import React, { useState, useEffect } from "react"
 import Container from "react-bootstrap/Container"
-
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 function Navbarr() {
+  const countProduct = useSelector((state) => state.cart)
+  const [scrolling, setScrolling] = useState(false)
+  const [hidden, setHidden] = useState(false)
+  let lastScrollTop = 0
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let scrollTop = window.scrollY
+
+      if (scrollTop > lastScrollTop && scrollTop > 50) {
+        setHidden(true) // Hide navbar when scrolling down
+      } else {
+        setHidden(false) // Show navbar when scrolling up
+      }
+
+      setScrolling(scrollTop > 50) // Change background after scrolling 50px
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar
+      expand="lg"
+      className={`navbar-custom ${scrolling ? "scrolled" : ""} ${
+        hidden ? "hidden" : ""
+      }`}
+    >
       <Container fluid>
         <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -21,7 +50,7 @@ function Navbarr() {
             <Nav.Link to="cart" as={Link}>
               {/* // The "as" prop makes it render as a React Router Link component.
               // This enables client-side navigation without a full page reload. */}
-              My Bag 0
+              My Bag {countProduct.length}
             </Nav.Link>
           </Navbar.Text>
         </Navbar.Collapse>
